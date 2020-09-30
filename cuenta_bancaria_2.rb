4ed67a14ce56b603973c0c2ffd23d2cedd8b54d7
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-09-30 15:52:56 -0300

    Added saldo method and some output tests

--- cuenta_bancaria_2.rb
+++ cuenta_bancaria_2.rb
@@ -1,4 +1,17 @@
 class Usuario
+    attr_accessor :cuentas
+    def initialize(nombre, cuentas)
+        @nombre = nombre
+        @cuentas = cuentas
+    end
+
+    def saldo(user)
+        saldo_total = 0
+        user.cuentas.each do |i|
+            saldo_total += i.saldo
+        end
+        return saldo_total
+    end
 end
 
 class CuentaBancaria
@@ -16,13 +29,27 @@ class CuentaBancaria
     end
 end
 
-#Testeando el método transferir
-
+#Para testear métodos 
 c1 = CuentaBancaria.new('Santander', 12345678, 5000)
 c2 = CuentaBancaria.new('Estado', 87654321, 5000)
+c3 = CuentaBancaria.new('VTR', 11111111, 5000)
+c4 = CuentaBancaria.new('BCI', 22222222, 5000)
 
-c1.transferir(5000, c2)
+#Testeando método transferir
+c1.transferir(500, c2)
 
-pp c1.saldo
-pp c2.saldo
+#Primer output test
+pp c1.saldo #=> 4500
+pp c2.saldo #=> 5500
 
+#Testeando método constructor de cuentas
+user1 = Usuario.new('Pancho', [c3, c4])
+
+#Output test
+pp user1
+
+#Testeando el método saldo de la clase Usuario
+total = user1.saldo(user1)
+
+#Segundo output test
+pp total
